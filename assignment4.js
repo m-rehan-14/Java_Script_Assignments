691fe62dff120821ae170418c953316216216959
m-rehan-14 <remyarttt@gmail.com>
2026-06-29 17:33:38 +0500

    assignment4.js

--- assignment4.js
+++ assignment4.js
@@ -1,27 +1,27 @@
 // Chapter: Vriable Names : Legal & Illegal
 // Q1= Declare 3 variables in one statement.
 
-// let userName = "john", carName = "Volvo", _score = 12;
-// console.log(userName, carName, _score);
+let userName = "john", carName = "Volvo", _score = 12;
+console.log(userName, carName, _score);
 
 
 // Q2= Declare 5 legal and 5 illegal variable names.
 
 // 5 legal variable names :
 
-// var userName;
-// var age;
-// var my_Name;
-// var $price;
-// var student123;
+var userName;
+var age;
+var my_Name;
+var $price;
+var student123;
 
 // 5 illegal variable names
 
-// var 123student;
-// var my name;
-// var user-name;
-// var @price;
-// var var;
+var 123student;
+var my name;
+var user-name;
+var @price;
+var var;
 
 
 // Q3= Display this in your browser
