26e3fff6fd3ef303d14b54fcbbba3f1a72767b85
m-rehan-14 <remyarttt@gmail.com>
2026-06-29 17:00:15 +0500

    assignment3.js

--- assignment3.js
+++ assignment3.js
@@ -1,21 +1,21 @@
 // Chapter:3 Variables For Numbers
 // Q1= Declare a variable called age & assign to it your age. Show your age in an alert box.
 
-// var age = 14;
+var age = 14;
 
-// alert("I am " + age + " years old.");
+alert("I am " + age + " years old.");
 
 
 // Q2= Declare & intializa a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: "You have visited this site N times".
 
-// var visits = 14;
-// alert("You have visited this site " + 14 + " times.");
+var visits = 14;
+alert("You have visited this site " + 14 + " times.");
 
 
 // Q3= Declare a variable called birth year & assign to it your birth year. Show the following message in your browser:
     // ... ... ... ... ... ...
 
-// document.write("My Birth Year is 1990");
+document.write("My Birth Year is 1990");
 
 
 // Q4= A visitor visits an online clothing Store www.xyzClothing.com . Write a script to store in variables the following information:
@@ -24,8 +24,8 @@
 //    c. Quantity i.e. how many products a visitor wants to order
 // show the following message in your browser: "John Doe ordered 5 T-shirt(s) on XYZ Clothing store".
 
-// var visitorName = "John Doe";
-// var productTitle = "T-shirt";
-// var Quantity = "5";
+var visitorName = "John Doe";
+var productTitle = "T-shirt";
+var Quantity = "5";
 
-// document.write(visitorName + " ordered " + Quantity + " " + productTitle + " (s) on XYZ Clothing store ");
+document.write(visitorName + " ordered " + Quantity + " " + productTitle + " (s) on XYZ Clothing store ");
