9074d767c3c5cdcdd35c6dc77c7f85c7db8a4fd4
m-rehan-14 <remyarttt@gmail.com>
2026-06-29 17:08:02 +0500

    assignment1.js

--- assignment1.js
+++ assignment1.js
@@ -1,18 +1,18 @@
 // Chapter:1 Alerts
 // Q1= Write a script to greet your website visitor using JS alert box.
 
-// alert("Welcome! to our website");
+alert("Welcome! to our website");
 
 
 // Q2= Write a script to display  following message on your web pageXOffset:
 //    ... ... ... ... ... 
 
-// alert("ERROR! Please enter a valid password.");
+alert("ERROR! Please enter a valid password.");
 
 
 // Q3= Write a script to display following message on your web page:(Hint : Use line break)
 
-// alert("Welcome to JS land...\nHappy Coding!");
+alert("Welcome to JS land...\nHappy Coding!");
 
 
 // Q4= SKIPPED
@@ -21,4 +21,4 @@
 // Q5= Generate the following message through browser's developer console:
 //   ... ... ... ... ... ...
 
-// alert("Hello... I can run JS through my web browser's console");
+alert("Hello... I can run JS through my web browser's console");
